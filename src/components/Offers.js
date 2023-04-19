2d4c62d00b07015c1cb808ba9e5fbe3ee6820af4
Louis <louis@planhub.ca>
2023-04-19 13:38:52 -0400

    creation structure Widget

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Card from './UI/Card';
 import classes from "./Offers.module.css";
 
-import airaloLogo from '../assets/logo/airaloLogo-100x100.png';
-import flexiroamLogo from '../assets/logo/flexiroamLogo-100x100.png';
+import airaloLogo from '../assets/logo/airalo-100x100.png';
+import flexiroamLogo from '../assets/logo/flexiroam-100x100.png';
 import nomad from '../assets/logo/nomad-100x100.png'
 
 const Offers = () => {
@@ -11,18 +11,21 @@ const Offers = () => {
         <div className={classes.proposal_plans}>
             <Card
                 logo={airaloLogo}
+                provider="Airalo"
             >
                 Offre 1
             </Card>
             
             <Card
                 logo={flexiroamLogo}
+                provider="Flexiroam"
             >
                 Offre 2
             </Card>
 
             <Card
                 logo={nomad}
+                provider="Nomad"
             >
                 Offre 3
             </Card>
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,32 @@
 import classes from "./Card.module.css"
 
 const Card = props => {
+    console.log(props)
+
     return (
         <div className={classes.card}>
-            {props.children}
+            
+            <div> {/* Container 1st line */}
+                <div> {/* Container 1st column */}
+                    <img src={props.logo} />
+                    <span>
+                        {props.provider}
+                    </span>
+                </div>
+                <div>
+                    <span> Plan Name </span>
+                    <ul>
+                        <li>Validite</li>
+                        <li>Prix</li>
+                        <li>Pays</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div> {/* Container 2nd line */}
+                <div>Capacity</div>
+                <span>Offer</span>
+            </div>
         </div>
     )
 }
