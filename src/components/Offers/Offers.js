99e06e69fab5036e1c8898ce5e580461230b0c36
Louis <louis@planhub.ca>
2023-04-20 16:01:45 -0400

    debuging

--- src/components/Offers/Offers.js
+++ src/components/Offers/Offers.js
@@ -1,45 +1,49 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+
 import Card from '../UI/Card';
 import CheckAllOffers from '../CheckAllOffers/CheckAllOffers'
 import classes from "./Offers.module.css";
 
-import airaloLogo from '../../assets/logo/airalo-100x100.png';
-import flexiroamLogo from '../../assets/logo/flexiroam-100x100.png';
-import nomad from '../../assets/logo/nomad-100x100.png'
-
 const Offers = () => {
+    const [offersData, setOffersData] = useState([]);
+
+    useEffect(() => {
+        fetch("offers.json")
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Error while fetching the data');
+                }
+            })
+            .then((dataJSON) => {
+                console.log(dataJSON);
+                setOffersData(dataJSON);
+            })
+            .catch((error) => {
+                console.error(error);
+                // console.log(response.body);
+            });
+    }, [])
+
+    const offersList = offersData.map((offer) => (
+        <Card
+            id={offer.id}
+            key={offer.id}
+            logo={offer.logo}
+            provider={offer.provider}
+            capacity={offer.capacity}
+            planName={offer.planName}
+            location={offer.country}
+            />
+    ));
+
     return (
         <div className={classes.mainContainer}>
             <div className={classes.proposal_plans}>
-                <Card
-                    logo={airaloLogo}
-                    provider="Airalo"
-                    capacity="1 GB"
-                    planName="Tuque Mobile - 1 GB"
-                    location="Canada"
-                >
-                    Offre 1
-                </Card>
-
-                <Card
-                    logo={flexiroamLogo}
-                    provider="Flexiroam"
-                    capacity="3 GB"
-                    planName="3GB Global Plan"
-                    location="Canada"
-                >
-                    Offre 2
-                </Card>
-
-                <Card
-                    logo={nomad}
-                    provider="Nomad"
-                    capacity="2 GB"
-                    planName="CANADA 2GB "
-                    location="Canada"
-                >
-                    Offre 3
-                </Card>
+                {offersList}
+                
             </div>
 
             <CheckAllOffers />
