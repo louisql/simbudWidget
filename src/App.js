3236ab6226bee565d7542d946ee4cce0ab08f125
Louis <louis@planhub.ca>
2023-04-21 13:51:28 -0400

    console.log

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Offers from './components/Offers/Offers';
 const App = () => {
   useEffect(() => {
     let communication = () => {
+      console.log("test communication")
       let targetOrigin = 'https://amazing-sundae-746c61.netlify.app/'; // replace with the domain of file2
       window.parent.postMessage('message', targetOrigin);
     };
