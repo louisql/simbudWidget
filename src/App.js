918099a54ddc4c944b82c1b51dcea56705ca7ae8
Louis <louis@planhub.ca>
2023-04-21 11:29:38 -0400

    debut implementation communication window.postmessage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import Offers from './components/Offers/Offers';
 
 function App() {
+
+  let communication = () => {
+    let url = 
+    window.location != window.parent.location
+      ? document.referrer
+      : document.location.href;
+    window.parent.postMessage("communication", url);
+  };
+
   return (
     <div className="App">
       <Offers />
